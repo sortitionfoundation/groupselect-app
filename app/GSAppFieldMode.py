ad3de849dd0aae822e1000c3d694d33e56a7ff96
Philipp Verpoort <philipp@verpoort.online>
2026-05-20 18:36:41 +0200

    Updated field mode names in groupselect-lib.

--- app/GSAppFieldMode.py
+++ app/GSAppFieldMode.py
@@ -6,20 +6,14 @@ from groupselect import FieldMode
 class GSAppFieldMode(Enum):
     Ignore = 0
     Diversify = 1
-    #Diversify_rank_2 = 2
-    #Diversify_rank_3 = 3
-    Cluster = 4
-    Display = 5
-    Label = 6
+    Cluster = 2
+    Display = 3
+    Label = 4
 
 
 def map_field_modes(field_mode: GSAppFieldMode):
     if field_mode == GSAppFieldMode.Diversify:
-        return FieldMode.Diversify_1
-   # elif field_mode == GSAppFieldMode.Diversify_rank_2:
-    #    return FieldMode.Diversify_2
-    #elif field_mode == GSAppFieldMode.Diversify_rank_3:
-     #   return FieldMode.Diversify_3
+        return FieldMode.Diversify
     elif field_mode == GSAppFieldMode.Cluster:
         return FieldMode.Cluster
     else:
